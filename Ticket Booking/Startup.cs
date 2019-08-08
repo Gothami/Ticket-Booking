7a12cb45abc99173ac09727d517f409966d61e39
gothami <gothamisa92@gmsil.com>
2019-08-08 18:48:30 +0930

    Add project files.

---
+++ Ticket Booking/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Ticket_Booking.Startup))]
+namespace Ticket_Booking
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

575b0a39b3b1109c4d270a48e9788ffc00bb555c
Gothami <Gothami@DESKTOP-KVNVJV0>
2020-02-19 22:52:38 +1030

    Add Unit Tests

---
+++ TicketBookingUnitTests/ControllerTest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ticket_Booking;
+using Ticket_Booking.Controllers;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+
+namespace TicketBookingUnitTests
+{
+    [TestClass]
+    public class ControllerTest
+    {
+        [TestMethod]
+        public void testIndex()
+        {
+            //Arrange
+            HomeController homeController = new HomeController();
+
+            //Act
+            ViewResult result = homeController.Index() as ViewResult;
+
+            //Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void testAbout()
+        {
+            //Arrange
+            HomeController homeController = new HomeController();
+
+            //Act
+            ViewResult result = homeController.About() as ViewResult;
+
+            //Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void testContact()
+        {
+            //Arrange
+            HomeController homeController = new HomeController();
+
+            //Act
+            ViewResult result = homeController.Contact() as ViewResult;
+
+            //Assert
+            Assert.IsNotNull(result);
+        }
+
+
+    }
+}
